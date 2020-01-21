0bab63868aad6a304020c217a80741019f58967f
Saharak <Saharak@bananacoding.com>
2020-01-21 12:57:27 +0700

    [Setup] Vue JS

---
+++ app/javascript/packs/vueapp.js
@@ -0,0 +1,11 @@
+import Vue from 'vue/dist/vue.esm';
+
+document.addEventListener('DOMContentLoaded', () => {
+	new Vue({
+		el: '#app',
+		data: {
+			message: 'Hello Vue!'
+		},
+		methods: {}
+	});
+});
